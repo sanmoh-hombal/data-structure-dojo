61e296db21cb60a18ced733e951df5a15918b171
Sanmoh Hombal <sanmoh.hombal@gmail.com>
2024-12-06 16:46:58 +0530

    test(Arrays): update StaticArrayTest for dynamic capacity handling 🚀
    Enhanced the `StaticArrayTest` to accommodate dynamic capacity by adjusting the instantiation of `StaticArray` and incorporating additional insertions. Updated test assertions to reflect changes in list size and indexed insertions.

--- src/test/java/org/sanmoh/Arrays/StaticArrayTest.java
+++ src/test/java/org/sanmoh/Arrays/StaticArrayTest.java
@@ -7,14 +7,19 @@ import org.junit.jupiter.api.*;
 public class StaticArrayTest {
   @Test
   public void insert() {
-    StaticArray array = new StaticArray(5);
+    StaticArray array = new StaticArray();
     array.insert(1, 0);
     array.insert(2, 1);
     array.insert(3, 2);
     array.insert(4, 3);
     array.insert(5, 4);
-    assertArrayEquals(new Integer[] {1, 2, 3, 4, 5}, array.list());
-    assertThrows(IndexOutOfBoundsException.class, () -> array.insert(6, 6));
+    array.insert(6, 5);
+    array.insert(7, 6);
+    array.insert(8, 7);
+    array.insert(9, 8);
+    array.insert(10, 9);
+    assertArrayEquals(new Integer[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, array.list());
+    assertThrows(IndexOutOfBoundsException.class, () -> array.insert(11, 10));
   }
 
   @Test
